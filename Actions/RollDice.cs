fa83e75f495f759e75eca267c681c3bf7993b632
Dmytro Bignyak <dmytro.bignyak@onapp.com>
2017-02-01 23:06:48 +0200

    Refactor roll dice

--- Actions/RollDice.cs
+++ Actions/RollDice.cs
@@ -6,12 +6,11 @@ namespace JewishBot.Actions
     internal class RollDice : IAction
     {
         private const string DefaultPatern = "1d6";
-        private readonly TelegramBotClient _bot;
-        private string _message;
+        private TelegramBotClient Bot { get; }
 
         public RollDice(TelegramBotClient bot)
         {
-            _bot = bot;
+            Bot = bot;
         }
 
         public async void HandleAsync(long chatId, string username, string[] args)
@@ -19,9 +18,9 @@ namespace JewishBot.Actions
             var toParse = (args != null && Dice.CanParse(args[0])) ? args[0] : DefaultPatern;
             var result = new Dice(toParse);
 
-            _message = result.GetSum().ToString();
+            var message = result.GetSum().ToString();
 
-            await _bot.SendTextMessageAsync(chatId, $"{username}: \uD83C\uDFB2 {_message}");
+            await Bot.SendTextMessageAsync(chatId, $"{username}: \uD83C\uDFB2 {message}");
         }
     }
 }
